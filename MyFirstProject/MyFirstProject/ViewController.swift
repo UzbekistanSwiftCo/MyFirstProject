13ed8e46f7aac49f5e714b67b5c35bc497b44e38
Shohzod <shohzodrajabovmir@gmail.com>
2023-02-25 21:38:06 +0500

    -firstCommit

---
+++ MyFirstProject/MyFirstProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstProject
+//
+//  Created by Shohzod Rajabov on 25/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+
+
+}
+
